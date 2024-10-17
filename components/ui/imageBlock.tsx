867f8da911ef9287fc2231c963763b3b03c9d90a
itka0526 <itka0526@gmail.com>
2024-10-17 14:25:51 +0300

    hospital elevator

--- components/ui/imageBlock.tsx
+++ components/ui/imageBlock.tsx
@@ -8,7 +8,7 @@ type ImageBlockProps = {
 
 export function ImageBlock({ alt, src, description }: ImageBlockProps) {
     return (
-        <div className="border border-slate-200 p-2 flex flex-col gap-4 hover:shadow-md">
+        <div className="border border-slate-200 p-2 flex flex-col gap-4 hover:shadow-md items-center">
             <Image src={src} alt={alt} height={256} width={256} />
             <div className="w-full text-center">
                 <span className="hover:text-blue-600 text-blue-500 ">{description}</span>
